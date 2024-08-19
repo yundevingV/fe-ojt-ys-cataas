7a9d3b343044162eebcf6d90f4bc27ba1fddf7f8
yundevingV <91381230+yundevingV@users.noreply.github.com>
2024-08-19 10:55:50 +0900

    Feat : 랜덤 태그 뽑기 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import Tag from "@/components/tag";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  function getRandomItems(arr: string[], count: number): string[] {
+    const shuffled = arr.sort(() => 0.5 - Math.random()); // 배열을 랜덤하게 섞음
+    return shuffled.slice(0, count); // 처음 8개의 요소를 반환
+  }
 
   const { data: tagsData, isLoading, error } = useQuery<GetTagsDTO>({
     queryKey: ['tags-data'], // 쿼리 키
@@ -19,9 +23,11 @@ export default function Home() {
     return <p>오류 발생: {error.message}</p>;
   }
 
+  const randomTags = tagsData ? getRandomItems(tagsData, 8) : []; // 랜덤으로 8개 선택
+
   return (
-    <div>
-      {tagsData?.map((item, index) => (
+    <div className="grid grid-cols-4 gap-4"> {/* Tailwind CSS 그리드 클래스 사용 */}
+      {randomTags?.map((item, index) => (
         <Tag key={index} content={item} /> 
       ))}
     </div>
