a2e4738cad45c6139db253336af26fc3393c64ac
yundevingV <91381230+yundevingV@users.noreply.github.com>
2024-08-26 13:49:10 +0900

    Feat : lazy loading 구현

--- components/Result/SearchImage.tsx
+++ components/Result/SearchImage.tsx
@@ -1,5 +1,6 @@
 import { CatDTO } from "@/api/cats/getCats";
 import ButtonTags from "@/components/Tags/ButtonTags";
+import useLazyLoad from "@/hooks/useLazyLoad";
 import Link from "next/link";
 import { useState, useRef, useEffect } from "react";
 
@@ -8,6 +9,9 @@ export interface SearchImageProps {
 }
 
 export default function SearchImage({ cats }: SearchImageProps) {
+
+  const { ref, isVisible } = useLazyLoad();
+
   const [isHovered, setIsHovered] = useState(false);
   const [, setSelectedTags] = useState<string[]>([]);
 
@@ -26,39 +30,30 @@ export default function SearchImage({ cats }: SearchImageProps) {
 
   const containerRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const checkHeight = () => {
-      if (containerRef.current) {
-        const { scrollHeight } = containerRef.current;
-        if (scrollHeight > 60) {
-
-        }
-      }
-    };
-
-    if (isHovered) {
-      checkHeight();
-    }
-  }, [cats.tags, isHovered]);
-
   return (
+    
     <div
+      ref={ref}
       className="relative cursor-pointer"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-    >
-      <Link href={`/detail/${cats._id}`}>
-        <img
-          src={`https://cataas.com/cat/${cats._id}`}
-          alt="고양이 이미지"
-          className="aspect-square rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-[1.03]" // 애니메이션 클래스 추가
-        />
-      </Link>
+      style={{ height: '400px', overflowY: 'hidden' }} // 초기 높이 설정
 
-      {isHovered && (
+    >
+      {isVisible ? (
+        <Link href={`/detail/${cats._id}`}>
+          <img
+            src={`https://cataas.com/cat/${cats._id}`}
+            alt="고양이 이미지"
+            className="aspect-square rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-[1.03]" // 애니메이션 클래스 추가
+          />
+        </Link>
+      ) : null}
+
+      {/* {isHovered && (
         <div
           ref={containerRef}
-          className="absolute bottom-0 left-0 right-0 p-2 shadow-lg text-ellipsis  "
+          className="absolute bottom-0 left-0 right-0 p-2 shadow-lg text-ellipsis"
           style={{
             background: 'linear-gradient(to top, rgba(20, 20, 20, 0.8) 4%, rgba(1, 1, 1, 0.5) 24%, rgba(1, 1, 1, 0.4) 100%)',
             maxHeight: '60px',
@@ -77,10 +72,9 @@ export default function SearchImage({ cats }: SearchImageProps) {
               active="active:bg-slate-300 hover:text-[#2f2f2f]"
             />
           ))}
-
         </div>
+      )} */}
 
-      )}
     </div>
   );
 }
